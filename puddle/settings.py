783f8532cd2da481d4bf960ea12bdc7eeaa77d8e
ustadji <naved232doom@gmail.com>
2023-08-23 23:54:51 +0530

    third attemp

--- puddle/settings.py
+++ puddle/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 import os
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
